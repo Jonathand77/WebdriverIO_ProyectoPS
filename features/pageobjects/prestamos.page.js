495678c6690878ab0c467d2757f6df4aa9a3b3b0
jonathanfdez77 <145861565+jonathanfdez77@users.noreply.github.com>
2025-07-08 18:31:18 -0500

    Create prestamos.page.js

---
+++ features/pageobjects/prestamos.page.js
@@ -0,0 +1,28 @@
+// features/pageobjects/prestamos.page.js
+import Page from './page.js';
+
+class PrestamosPage extends Page {
+    get linkLoan () { return $('=Request Loan'); }
+    get inputMonto () { return $('input[name="amount"]'); }
+    get inputCuotaInicial () { return $('input[name="downPayment"]'); }
+    get selectCuenta () { return $('select[name="fromAccountId"]'); }
+    get btnAplicar () { return $('input[value="Apply Now"]'); }
+    get mensajeResultado () { return $('#rightPanel h1'); }
+
+    async irASolicitudPrestamo() {
+        await this.linkLoan.click();
+    }
+
+    async solicitarPrestamo(monto, cuota, cuentaIdx = 1) {
+        await this.inputMonto.setValue(monto);
+        await this.inputCuotaInicial.setValue(cuota);
+        await this.selectCuenta.selectByIndex(cuentaIdx);
+        await this.btnAplicar.click();
+    }
+
+    async obtenerMensaje() {
+        return await this.mensajeResultado.getText();
+    }
+}
+
+export default new PrestamosPage();
